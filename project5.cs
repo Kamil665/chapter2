0a8418feff4caf1f42d86da6b3877b06fc6d40ac
Kamil665 <pochtung@gmail.com>
2025-11-11 15:10:50 +0300

    Create project5.cs

---
+++ project5.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program {
+    static void Main() {
+        int days = 234;
+        
+        int weeks = days / 7;
+        
+        Console.WriteLine($"Число дней: {days}");
+        Console.WriteLine($"Число полных недель: {weeks}");
+    }
+}
